493df7f78341a1b1008beac24b0e872870ecfc68
xjboss <i@xjboss.net>
2019-04-03 01:15:33 +0100

    fix

--- MusicBot/start_nm$l.sh
+++ MusicBot/start_nm$l.sh
@@ -5,7 +5,6 @@ set APP_HOME=$BOT_HOME
 set APP_OPTS=$BOT_OPTS
 set APP_JAR=$JMUSICBOT_APP
 cd $APP_HOME
-if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
 
   # read JAVA_OPTS and BOT_OPTS into arrays to avoid need for eval (and associated vulnerabilities)
   java_opts_array=()
@@ -25,8 +24,4 @@ if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
     app_opts_array+=( "$item" )
   done < <([[ $APP_OPTS ]] && xargs printf '%s\0' <<<"$APP_OPTS")
   cd 
-  exec java -Duser.home="$APP_HOME" "${java_opts_array[@]}" -jar ${APP_JAR} "${app_opts_array[@]}" "$@"
-fi
-
-# As argument is not jenkins, assume user want to run his own process, for example a `bash` shell to explore this image
-exec "$@"
+  exec java -Duser.home="$APP_HOME" "${java_opts_array[@]}" -jar $APP_JAR "${app_opts_array[@]}" "$@"
